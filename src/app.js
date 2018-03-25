837f9ef8bf5b1c51dc0b1e644b883b06f9f01940
Mathías <mathiasd88@gmail.com>
2018-03-25 04:25:16 -0300

    Cambio en estructura y funcionamiento general: Se agregó opción para crear directorio en base a estructura de carpetas dentro del package. Funcionamiento básico

--- src/app.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-let args = process.argv.splice(process.execArgv.length + 2);
-
-let name = args[0];
-
-console.log(name);
---
+++ src/index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+const fs = require('fs-extra');
+const path = require('path');
+
+const args = process.argv.splice(process.execArgv.length + 2);
+
+const action = args[0];
+const name = args[1];
+
+if (action === 'new') {
+    console.log(`Creating new project ${name}`);
+
+    fs.copy(__dirname + '/structure', name)
+      .then(() => console.log('Success!'))
+      .catch(err => console.error(err))
+}
